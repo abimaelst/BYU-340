68f5c5d092d5f76868e6c358a3e9a763d99723e8
Abimael <tavares@abimael.dev.br>
2025-09-27 19:02:19 +0200

    refactor error handling and update error page layout

--- server.js
+++ server.js
@@ -24,7 +24,8 @@ app.set("view engine", "ejs");
 app.use(expressLayouts);
 app.set("layout", "./layouts/layout");
 
-app.get("/", baseController.buildHome);
+// Index route
+app.get("/", utilities.handleErrors(baseController.buildHome));
 
 // Inventory routes
 app.use("/inv", inventoryRoute);
@@ -41,9 +42,14 @@ app.use(async (req, res, next) => {
 app.use(async (err, req, res, next) => {
   let nav = await utilities.getNav();
   console.error(`Error at: "${req.originalUrl}": ${err.message}`);
+  if (err.status == 404) {
+    message = err.message;
+  } else {
+    message = "Oh no! There was a crash. Maybe try a different route?";
+  }
   res.render("errors/error", {
     title: err.status || "Server Error",
-    message: err.message,
+    message,
     nav,
   });
 });
