8bfce80af989d74c18d42f844bc022cae615a707
Abimael <tavares@abimael.dev.br>
2025-09-27 18:49:44 +0200

    add custom error page and styles for 404 errors

--- server.js
+++ server.js
@@ -5,6 +5,7 @@
 
 const baseController = require("./controllers/baseController");
 const inventoryRoute = require("./routes/inventoryRoute");
+const utilities = require("./utilities/");
 
 /* ***********************
  * Require Statements
@@ -23,8 +24,29 @@ app.set("view engine", "ejs");
 app.use(expressLayouts);
 app.set("layout", "./layouts/layout");
 
+app.get("/", baseController.buildHome);
+
 // Inventory routes
 app.use("/inv", inventoryRoute);
+
+// File Not Found Route - must be last route in list
+app.use(async (req, res, next) => {
+  next({ status: 404, message: "Sorry, we appear to have lost that page." });
+});
+
+/* ***********************
+ * Express Error Handler
+ * Place after all other middleware
+ *************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav();
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`);
+  res.render("errors/error", {
+    title: err.status || "Server Error",
+    message: err.message,
+    nav,
+  });
+});
 
 /* ***********************
  * Local Server Information
@@ -32,8 +54,6 @@ app.use("/inv", inventoryRoute);
  *************************/
 const port = process.env.PORT;
 const host = process.env.HOST;
-
-app.get("/", baseController.buildHome);
 
 /* ***********************
  * Log statement to confirm server operation
