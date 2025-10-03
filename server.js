bd6f5418b4125f98d1e0c21b3a2438a6b175d653
Abimael <tavares@abimael.dev.br>
2025-10-03 05:15:55 +0200

    feat: add login view and route

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ const session = require("express-session");
 const pool = require("./database/");
 const baseController = require("./controllers/baseController");
 const inventoryRoute = require("./routes/inventoryRoute");
+const accountRoute = require("./routes/accountRoute");
 const utilities = require("./utilities/");
 
 app.use((req, res, next) => {
@@ -65,6 +66,9 @@ app.get("/", utilities.handleErrors(baseController.buildHome));
 
 // Inventory routes
 app.use("/inv", inventoryRoute);
+
+// Account routes
+app.use("/account", accountRoute);
 
 // File Not Found Route - must be last route in list
 app.use(async (req, res, next) => {
